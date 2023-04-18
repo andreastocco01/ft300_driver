7b625b8e1bb276581d7e7a3072422afe4bd290a4
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-04-18 10:19:09 +0200

    Remade whole parsing function. Now parsing is done independently from reading rate, but instead by checking brackets on the recived string. Incomplete packets now will be saved and later composed to reconstruct the original reading.

--- scripts/robotiq_ft_sensor_publisher.py
+++ scripts/robotiq_ft_sensor_publisher.py
@@ -1,7 +1,7 @@
-import rospy
-import re
 import socket
-from geometry_msgs.msg import WrenchStamped, Wrench
+
+import rospy
+from geometry_msgs.msg import Wrench, WrenchStamped
 from std_msgs.msg import Header
 
 REMOTE_IP = "192.168.53.3"
@@ -10,8 +10,8 @@ REMOTE_ADDR = (REMOTE_IP, REMOTE_PORT)
 
 def main():
     # Create socket and connect to remote address
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(REMOTE_ADDR)
+    local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    local_socket.connect(REMOTE_ADDR)
 
     # Create a publisher and a new node in ROS
     publisher = rospy.Publisher('sensor_topic', WrenchStamped, queue_size=1)
@@ -19,53 +19,83 @@ def main():
     # Set rate to run (100hz for robotiq)
     rate = rospy.Rate(100)
 
+    # Create message parts
+    wrench_message = WrenchStamped()
+    header = Header()
+    wrench = Wrench()
+    header.frame_id = "robotiq_ft_frame_id"
+
+    # Create a varaible to hold incomplete packets
+    last_message = ""
     while not rospy.is_shutdown():
 
-        # Read 71 bytes from tcp stream
-        buffer, addr = sock.recvfrom(4096)
-        #packets = re.search("(?=\()(.*)(?<=\))", buffer.decode("ascii"))
-        
-        packet = str(buffer, "ascii").replace(" ", "").replace("(", "").replace(")", "").split(",")
-        print(packet)
-        
-        # Convert values to float numbers
-        fx = packet[0]
-        fy = packet[1]
-        fz = packet[2]
-        mx = packet[3]
-        my = packet[4]
-        mz = packet[5]
+        # Read bytes from tcp stream and convert to ascii string
+        buffer, addr = local_socket.recvfrom(100)
+        message = buffer.decode("ascii")
 
-        # DEBUG: print float values
-        #print(f"{{ {fx}, {fy}, {fz}, {mx}, {my}, {mz} }}")
-        #print("\n\n") 
+        # We want to split the message in packets, but we need to track the peackets incomplete '(...' or '...)'
+        message = message.replace(")", ")|")    # So we add another delimiter between packets
+        reading_list = message.split("|")       # Now we split using our delimiter, which will be removed
 
-        # Create message parts
-        message = WrenchStamped()
-        header = Header()
-        wrench = Wrench()
-        
-        # Construct header by setting frame_id and stamp
-        header.frame_id = "robotiq_ft_frame_id"
-        header.stamp = rospy.Time.now()
+        # We can now inspect the first packet
+        if (reading_list[0].count("(") == 0):
+            # if the first packet doesn't have the opening braket, we compose it with the last packet of the previus reading from socket
+            reading_list[0] = last_message + reading_list[0]
+            last_message = "" #last message has been used, we clean it
+        # Now we inspect the last packet
+        if (reading_list[-1].count(")") == 0):
+            # if the last packet doesn't have the closing bracket, we save it for later use
+            last_message = reading_list[-1]
+            reading_list.remove(reading_list[-1]) # and we remove the incomplete packet from the reading list. It will be used in the next reading.
 
-        # Construct force vector
-        wrench.force.x = fx
-        wrench.force.y = fy
-        wrench.force.z = fz
-        # Construct torque vector
-        wrench.torque.x = mx
-        wrench.torque.y = my
-        wrench.torque.z = mz
+        # Now we scan each packet
+        for reading in reading_list:
+            
+            # Make sure each reading is now fully composed
+            if reading.count("(") == 0 or reading.count(")") == 0:
+                rospy.loginfo(f"MALFORMED packet is missing brackets, printing buffer, list and readings:\n" +
+                              f"BUFFER:{message}\n\n" +
+                              f"Reading list:{reading_list}\n\n" +
+                              f"Reading:{reading}")
+                # We don't send this reading
+                continue
 
-        # Set message header and wrench
-        message.header = header
-        message.wrench = wrench
+            # We can finally remove brakets
+            reading = reading.replace("(", "").replace(")", "")
+            # Now we can split safely, and convert each value to float value
+            values = list(map(float, reading.split(" , ")))
+            
+            # We make sure each packet has 6 values
+            if len(values) != 6 :
+                rospy.loginfo(f"MALFORMED packet is missing values, printing buffer, list and readings:\n" +
+                              f"BUFFER:{message}\n\n" +
+                              f"Reading list:{reading_list}\n\n" +
+                              f"Reading:{reading}\n" +
+                              f"has been mapped to\n" +
+                              f"{values}")
+                # We don't send this reading
+                continue
 
-        # DEBUG: log values
-        #rospy.loginfo(packet)
-        # Publish WrenchStamped message to topic and sleep
-        publisher.publish(message)
+            # Now we need to construct the message 
+            # Construct force vector
+            wrench.force.x = values[0]
+            wrench.force.y = values[1]
+            wrench.force.z = values[2]
+            #Construct torque vector
+            wrench.torque.x = values[3]
+            wrench.torque.y = values[4]
+            wrench.torque.z = values[5]
+            # Construct header's stamp
+            header.stamp = rospy.Time.now()
+
+            # Set message header and wrench
+            wrench_message.header = header
+            wrench_message.wrench = wrench
+
+            # Publish WrenchStamped message to topic and sleep
+            publisher.publish(wrench_message)
+
+        # We sent packets received, we can read again at given ros frequency
         rate.sleep()
 
 
